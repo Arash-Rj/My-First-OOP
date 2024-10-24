3539e0c6b41d1942a0466cfa5da91685f39d1017
Arash <rajabiarash36@gmail.com>
2024-10-24 10:46:22 +0330

    First commit

---
+++ HW41/Class1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Book
+{
+    public string Feild { get; set; }
+    private string ID;
+    public string Id
+    { 
+        get
+        {
+             string id = "Num";
+            for (int i = 0; i < Title.Length; i++)
+            {
+                id += Convert.ToInt32(Title[i]).ToString();
+            }
+            ID = id;
+            return ID;
+        }
+        
+        set
+        {}
+       
+    }
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public int yearpublished { get; set; }
+    public string Edition {  get; set; }
+    public int pages {  get; set; }
+
+    private int Price; 
+    public int price
+    {
+        get 
+        {
+            Price = pages * 1000 + 100000;
+             return Price;  
+        }
+        set
+        { }
+    }
+    
+    public Book(string feild,string author,string title,int Yearpublished,string edition,int Pages)
+    {
+        Feild = feild;
+        Title = title;
+        yearpublished = Yearpublished;
+        Edition = edition;
+        pages = Pages;
+        Author = author.ToUpper();
+    }
+    public int priceincrease(int x)
+    {
+        Price += x;
+        return Price;
+    }
+    public void changeEdition(string newEdition)
+    {
+        Edition = newEdition;
+    }
+    public void changepages(int newamount)
+    {
+        pages = newamount;
+    }
+    //public string titlebyauthor(string Author1)
+    //{
+    //    string A = Author1.ToUpper();
+    //    if( Author == A)
+    //    {
+    //        return Title;
+    //    }
+    //    return "";
+    //}
+    public string getbookinfo()
+    {
+      string Info ="";
+        Info += $" Title: {Title},    " +
+            $"Author: {Author},    " +
+            $"Edition: {Edition},    " +
+            $"PublishedYear: {yearpublished},    " +
+            $"Number of Pages: {pages}";
+        return Info;
+    }
+  
+
+
+
+}
---
+++ HW41/Program.cs
@@ -0,0 +1,61 @@
+﻿bool A = false;
+int i = 0;
+Book[] Books = new Book[100];
+while (!A)
+{
+    Console.WriteLine("---------Give me your book information.--------");
+    Console.Write("  Feild: ");
+    string Feild = Console.ReadLine();
+    Console.Write("  Author: ");
+    string Author = Console.ReadLine();
+    Console.Write("  Title: ");
+    string Title = Console.ReadLine();
+    Console.Write("  Year published: ");
+    int Publishedyear = Convert.ToInt32(Console.ReadLine());
+    Console.Write("  Edition: ");
+    string Edition = Console.ReadLine();
+    Console.Write("  Number of pages: ");
+    int Pages = Convert.ToInt32(Console.ReadLine());
+    Book Book = new(Feild,Author,Title,Publishedyear,Edition,Pages);
+    Books[i] = Book;
+    i++;
+    Console.Write("Do you wanna add new book?(y/n) ");
+    string An = Console.ReadLine();
+    if (An == "n")
+    { 
+        break;
+    }
+
+}
+Book[] Books1 = new Book[i];
+for(int b=0;b<i;b++)
+{
+    Books1[b] = Books[b];
+}
+Console.Write("witch Author's book do you wish to see? ");
+string Author1 = Console.ReadLine();
+Book[] AuthorBooks =SearchBookbyAuthor(Author1,Books1); 
+    Book[] SearchBookbyAuthor(string Author, Book[] Book)
+{
+    int count = 0;
+    Book[] books = new Book[Book.Length];
+    foreach (Book book in Book)
+    {
+       if (Author == book.Author)
+        {
+            books[count] = book;
+            count++;
+        }
+    }
+    Book[] AuthorBooks = new Book[count];
+    for(int b=0;b<count;b++)
+    {
+        AuthorBooks[b] = books[b];  
+    }
+    return AuthorBooks;
+}
+
+foreach(Book book in AuthorBooks)
+{
+    Console.WriteLine(book.Title);
+}
